7c68b21dff77175d8a1e618fe41f98cb84fda4c9
Raghu <raghu.esh@gmail.com>
2025-03-12 14:11:28 +0530

    Create ec2.tf

---
+++ modules/ec2_instance/ec2.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+
+resource "aws_instance" "Web-Server01" {
+  ami           = "ami-005e54dee72cc1d00"
+  instance_type = "t2.micro"
+  associate_public_ip_address = false
+
+  tags = var.default_tags
+}
